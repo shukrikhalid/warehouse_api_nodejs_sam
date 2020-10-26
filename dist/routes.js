8913f1d5b87ffddd62e5705614de5650c495fe39
shukri khalid <shukri.khalid@maevi.my>
2020-10-26 16:15:28 +0800

    update

--- dist/routes.js
+++ dist/routes.js
@@ -11,7 +11,17 @@ var _express = require("express");
 
 var _warehouseController = _interopRequireDefault(require("./controllers/warehouse-controller"));
 
+var _authController = _interopRequireDefault(require("./controllers/auth-controller"));
+
 var router = (0, _express.Router)();
+router.post('/auth/login', _authController.default.login);
+router.post('/auth/logout', _authController.default.logout);
+router.post('/auth/signup', _authController.default.signup);
+router.post('/auth/confirm_signup', _authController.default.confirmSignup);
+router.post('/auth/forgot_password', _authController.default.forgotPassword);
+router.post('/auth/confirm_forgot_password', _authController.default.confirmForgotPassword);
+router.post('/auth/change_password', _authController.default.changePassword);
+router.post('/auth/refresh_token', _authController.default.refressToken);
 router.get('/warehouses', _warehouseController.default.getAll);
 router.post('/warehouses', _warehouseController.default.add);
 router.get('/warehouses/:WarehouseId', _warehouseController.default.get);
